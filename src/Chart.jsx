e595f6788533c43c6e52a08ec045a934a8688702
Mariana Villamizar <marianaviro93@gmail.com>
2025-02-13 19:15:12 -0500

    Added hover effect and tooltip

--- src/Chart.jsx
+++ src/Chart.jsx
@@ -2,25 +2,23 @@ import * as d3 from "d3";
 import { useState } from "react";
 
 export function Chart({ data }) {
-  const marginLeft = 100;
-  const width = 800;
+  const marginLeft = 230;
+  const width = 1200;
   const height = 600;
-  const marginRight = 40;
+  const marginRight = 230;
   const marginTop = 20;
   const marginBottom = 20;
 
   const heightBound = height - marginTop - marginBottom;
   const widthBound = width - marginLeft - marginRight;
 
-  const pop_data = data
-    .map((d) => {
-      return {
-        species: d["Common_name"],
-        year: d["Year"],
-        population: d["Population"],
-      };
-    })
-    .sort((a, b) => b.area - a.area);
+  const pop_data = data.map((d) => {
+    return {
+      species: d["Common_name"],
+      year: d["Year"],
+      population: d["Population"],
+    };
+  });
 
   const years = pop_data.map((d) => d.year);
   const pops = pop_data.map((d) => d.population);
@@ -31,14 +29,20 @@ export function Chart({ data }) {
   }));
 
   // const hake = bySpecies.get("South Pacific hake / Peruvian hake");
-
-  const filtered = species.filter(
-    (d) => d.name !== "South Pacific hake / Peruvian hake"
-  );
-  console.log(filtered);
+  // const filtered = species.filter(
+  //   (d) => d.name !== "South Pacific hake / Peruvian hake"
+  // );
   const show = true;
 
-  // const [hovered, setHovered] = useState(null);
+  const [hovered, setHovered] = useState(null);
+  const [tooltip, setTooltip] = useState({
+    show: false,
+    x: 0,
+    y: 0,
+    text: null,
+  });
+
+  const colors = d3.scaleOrdinal(d3.schemeCategory10);
 
   const xAxis = d3
     .scaleLinear()
@@ -47,15 +51,45 @@ export function Chart({ data }) {
 
   const yAxis = d3
     .scaleLog()
-    .domain([1, d3.max(pops)]) // log(0) = Infinity see example here: https://d3js.org/d3-scale/log
+    .domain([1, d3.max(pops)])
     .range([heightBound, marginTop]);
 
   const lineBuilder = d3
     .line()
     .x((d) => xAxis(d.year))
-    .y((d) => (yAxis(d.population) > 0 ? yAxis(d.population) : 1)); //added conditional statement for when population =0
+    .y((d) => (yAxis(d.population) > 0 ? yAxis(d.population) : yAxis(1)))
+    .curve(d3.curveBasis);
 
-  // const linePath = lineBuilder(pop_data);
+  const handleMouseOver = (event, d) => {
+    const [mouseX, mouseY] = d3.pointer(event);
+    const year = Math.round(xAxis.invert(mouseX));
+    const dataPoint = d.find((p) => p.year === year);
+    if (dataPoint) {
+      console.log(dataPoint);
+      setTooltip({
+        show: true,
+        x: mouseX,
+        y: mouseY,
+        text: {
+          species: dataPoint.species,
+          population: Math.round(dataPoint.population),
+          year: dataPoint.year,
+        },
+      });
+    }
+  };
+  const handleMouseMove = (event) => {
+    const [mouseX, mouseY] = d3.pointer(event);
+    setTooltip((prevTooltip) => ({
+      ...prevTooltip,
+      x: mouseX,
+      y: mouseY,
+    }));
+  };
+
+  const handleMouseLeave = () => {
+    setTooltip({ show: false, x: 0, y: 0, text: "" });
+  };
 
   return (
     <div>
@@ -63,7 +97,7 @@ export function Chart({ data }) {
       <svg width={width} height={height}>
         {/* Render/ Draw the visualization */}
         <g transform={`translate(0,0)`}>
-          {species.map((d) => {
+          {species.map((d, i) => {
             if (show) {
               return (
                 <path
@@ -71,24 +105,22 @@ export function Chart({ data }) {
                   height={heightBound}
                   key={d.name}
                   d={lineBuilder(d.datapoints)}
-                  stroke="#000000"
+                  stroke={colors(i)}
                   fill="none"
-                  strokeWidth={2}
+                  strokeWidth={4}
+                  strokeOpacity={hovered && d.name !== hovered ? 0.1 : 1}
+                  onMouseOver={(e) => {
+                    setHovered(d.name);
+                    handleMouseOver(e, d.datapoints);
+                  }}
+                  onMouseLeave={() => {
+                    setHovered(null);
+                    handleMouseLeave();
+                  }}
+                  onMouseMove={(e) => handleMouseMove(e)}
+                  style={{ cursor: "pointer" }}
                 />
               );
-            } else {
-              if (d.name !== "South Pacific hake / Peruvian hake")
-                return (
-                  <path
-                    width={widthBound}
-                    height={heightBound}
-                    key={d.name}
-                    d={lineBuilder(d.datapoints)}
-                    stroke="#000000"
-                    fill="none"
-                    strokeWidth={2}
-                  />
-                );
             }
           })}
         </g>
@@ -142,13 +174,48 @@ export function Chart({ data }) {
                   y={yAxis(tick)}
                   key={tick}
                 >
-                  {/* Format ticks see examples here: https://d3js.org/d3-format#locale_format */}
                   {yAxis.tickFormat(5, "s")(tick)}
                 </text>
               </g>
             );
           })}
         </g>
+        {/* Tooltip */}
+        {tooltip.show && (
+          <g transform={`translate(${tooltip.x},${tooltip.y})`}>
+            <rect
+              x="10"
+              y="-32"
+              width="200"
+              height="100"
+              fill="white"
+              stroke="black"
+              rx="5"
+              ry="5"
+              pointerEvents="none"
+            />
+            <text x="15" y="-20" fontSize="11px" pointerEvents="none">
+              <tspan x="15" dy="0" stroke="black">
+                Species:
+              </tspan>{" "}
+              <tspan x="15" dy="1.3em">
+                {tooltip.text.species}
+              </tspan>{" "}
+              <tspan x="15" dy="1.4em" stroke="black">
+                Population:
+              </tspan>{" "}
+              <tspan x="15" dy="1.3em">
+                {tooltip.text.population}
+              </tspan>{" "}
+              <tspan x="15" dy="1.4em" stroke="black">
+                Year:
+              </tspan>{" "}
+              <tspan x="15" dy="1.3em">
+                {tooltip.text.year}
+              </tspan>{" "}
+            </text>
+          </g>
+        )}
       </svg>
     </div>
   );
